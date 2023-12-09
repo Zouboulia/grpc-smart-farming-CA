f2dd4ca2cacf0e70056142373a9abf7f28ca5bc0
Zouboulia <78475889+Zouboulia@users.noreply.github.com>
2023-12-09 19:37:08 +0000

    created client and server code. Modified proto code. Added package in proto. Still errors on client response {}

--- client/app.js
+++ client/app.js
@@ -6,10 +6,31 @@ var protoLoader = require("@grpc/proto-loader");
 // Load the protobuf definition
 var PROTO_PATH = __dirname + "/protos/cropHealth.proto";
 var packageDefinition = protoLoader.loadSync(PROTO_PATH);
-var cropHealth_proto = grpc.loadPackageDefinition(packageDefinition).cropHealth;
+var cropHealth_proto = grpc.loadPackageDefinition(packageDefinition).farming;
 
 // Create a gRPC client
-var client = new proto.CalcService(
-  "0.0.0.0:40000",
+var client = new cropHealth_proto.SatelliteCropHealthMonitoring(
+  "localhost:40000",
   grpc.credentials.createInsecure()
 );
+
+// Get the location and crop type from the user
+var location = readlineSync.question("Enter the location of the crop field: ");
+var cropType = readlineSync.question("Enter the type of crop: ");
+
+// Create the request payload in order to make the gRPC call
+var payload = {
+  location: location,
+  crop_type: cropType,
+};
+
+// Make the gRPC call
+client.AnalyzeCropHealth(payload, function (err, response) {
+  // Callback function to receive the response from the server
+  if (err) {
+    // If an error occurs, log the error
+    console.error(err);
+  } else {
+    console.log(response); // Log the response from the server
+  }
+});
--- server/app.js
+++ server/app.js
@@ -1,14 +1,42 @@
-var grpc = require("@grpc/grpc-js")
-var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/proto.proto"
-var packageDefinition = protoLoader.loadSync(
-  PROTO_PATH
-)
-var proto = grpc.loadPackageDefinition(packageDefinition).package_name
+// Import necessary modules
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
 
+// Load the protobuf definition
+var PROTO_PATH = __dirname + "/protos/cropHealth.proto";
+var packageDefinition = protoLoader.loadSync(PROTO_PATH);
+var cropHealth_proto = grpc.loadPackageDefinition(packageDefinition).farming;
 
+// Create a gRPC server
+var server = new grpc.Server();
 
-var server = new grpc.Server()
-server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
-  server.start()
-})
+// Implement the RPC method AnalyzeCropHealth as defined in the proto file
+server.addService(cropHealth_proto.SatelliteCropHealthMonitoring.service, {
+  AnalyzeCropHealth: analyzeCropHealth,
+});
+
+// Define the AnalyzeCropHealth RPC method
+function analyzeCropHealth(call, callback) {
+  console.log("Received request:", call.request); // Log the request
+
+  var location = call.request.location;
+  var cropType = call.request.crop_type;
+
+  // For testing, crop always healthy
+  var healthStatus = "Healthy";
+
+  // Respond with health status
+  var response = { healthStatus: healthStatus };
+  console.log("Server Response:", response); // Log the response
+  callback(null, response);
+}
+
+// Start the server
+server.bindAsync(
+  "0.0.0.0:40000",
+  grpc.ServerCredentials.createInsecure(),
+  function () {
+    console.log("Server running successfully at 40000");
+    server.start();
+  }
+);
